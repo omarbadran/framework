b0b0fcd7f9009a816707045e27a2243668d2b706
Omar Badran <omarbadran.dev@gmail.com>
2019-10-13 00:36:27 +0300

    Update Gulp Configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 
-const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
@@ -8,6 +7,7 @@ const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const sassGlob = require('gulp-sass-glob');
 
 
 /**
@@ -15,33 +15,41 @@ const cssnano = require('cssnano');
  */
 const files = { 
     sass: {
-        src:    'assets/scss/**/*.scss',
+        src:    'assets/scss/*.scss',
+        watch:  'assets/scss/**/*.scss',
         dist:   'assets/css'
     },
     js: {
-        src:    ['assets/js/**/*.js', '!assets/js/app.min.js'],
+        src:    ['assets/js/*.js', '!assets/js/app.min.js'],
+        watch:  ['assets/js/*.js', '!assets/js/app.min.js'],
         dist:   'assets/js'
     }
 }
 
 /**
- * Styles
+ * scss
  */
-function styles () {    
+gulp.task('scss' , function () {
+
+    console.log('Compiling SCSS.');
+    
     return gulp.src(files.sass.src)
-        .pipe(sourcemaps.init())
+        .pipe(sassGlob())
         .pipe(sass())
         .pipe(postcss([ autoprefixer(), cssnano() ]))
-        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest(files.sass.dist)
     );
-}
+
+});
 
 
 /**
- * Scripts
+ * js
  */
-function scripts () {
+gulp.task('js' , function () {
+
+    console.log('Compiling JS.');
+
     return gulp.src(files.js.src)
         .pipe(concat('app.min.js'))
         .pipe(babel({
@@ -50,19 +58,22 @@ function scripts () {
         .pipe(uglify())
         .pipe(gulp.dest(files.js.dist)
     );
-}
+
+});
 
 
 /**
  * Watch for changes
  */
-function watch () {
-    gulp.watch(files.sass.src, styles);
-    gulp.watch(files.js.src, scripts);
-}
+gulp.task('watch' , function () {
+    console.log('Watching for changes.');
+    
+    gulp.watch(files.sass.watch, gulp.series('scss'));
+    gulp.watch(files.js.watch, gulp.series('js'));
+    
+    return;
+});
 
 
 
-exports.styles = styles;
-exports.scripts = scripts;
-exports.watch = watch;
+exports.default = gulp.series('scss','js','watch');
